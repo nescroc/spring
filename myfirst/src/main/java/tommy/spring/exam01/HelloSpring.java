095a784bcbe97f796070bd2c2ba6d47557c7f671
NIS <nes0510@gmail.com>
2021-09-23 20:36:37 +0900

    제1강 20210923 myfirst

---
+++ myfirst/src/main/java/tommy/spring/exam01/HelloSpring.java
@@ -0,0 +1,8 @@
+package tommy.spring.exam01;
+
+public class HelloSpring {
+	public static void main(String[] args) {
+		MyBean myBean = new MyBean();
+		myBean.sayHello("fhkdlftjd");
+	}
+}
---
+++ myfirst/src/main/java/tommy/spring/exam03/MyBeanOne.java
@@ -0,0 +1,9 @@
+package tommy.spring.exam03;
+
+public class MyBeanOne implements MyBean {
+	
+	@Override
+	public void sayHello(String name) {
+		System.out.println("æ»≥Á«œººø‰ "+name+" !");
+	}
+}
